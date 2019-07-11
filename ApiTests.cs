4abdb1d257563a06fcbb3259cf42fa11c2dbe95c
john1506 <jad.nolan@outlook.com>
2019-07-11 14:58:45 +0100

    Initial commit

---
+++ ApiTests.cs
@@ -0,0 +1,87 @@
+using ApiTests.ModelObjects;
+using NUnit.Framework;
+using RestSharp;
+using System.Net;
+
+namespace Tests
+{
+    public class ApiTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+       
+
+        [TestCase("http://jsonplaceholder.typicode.com/", "posts", Method.GET, @"application/json; charset=utf-8", TestName = "Check Content Type is application/json")] 
+        public void ContentTypeAndStatusCodeTest(string baseUrl, string requestUrl, Method requestMethod, string expectedContentType)
+        {
+            // arrange
+            RestClient client = new RestClient(baseUrl);
+            RestRequest request = new RestRequest(requestUrl, requestMethod);
+
+            // act
+            IRestResponse response = client.Execute(request);
+
+            // assert            
+            Assert.That(response.ContentType, Is.EqualTo(expectedContentType));
+        }
+
+        [TestCase("http://jsonplaceholder.typicode.com/", "posts", Method.GET, HttpStatusCode.OK, TestName = "Get Post Status Code OK")]
+        [TestCase("http://jsonplaceholder.typicode.com/", "posts", Method.POST, HttpStatusCode.Created, TestName = "Create Post Status Code Created")]
+        public void StatusCodeTest(string baseUrl, string requestUrl, Method requestMethod, HttpStatusCode expectedHttpStatusCode)
+        {
+            // arrange
+            RestClient client = new RestClient(baseUrl);
+            RestRequest request = new RestRequest(requestUrl, requestMethod);
+
+            // act
+            IRestResponse response = client.Execute(request);
+
+            // assert
+            Assert.That(response.StatusCode, Is.EqualTo(expectedHttpStatusCode));            
+        }
+
+        [TestCase("http://jsonplaceholder.typicode.com/", "posts", Method.POST, HttpStatusCode.Created, @"application/json; charset=utf-8", TestName = "Create Post")]
+        public void CreatePost(string baseUrl, string requestUrl, Method requestMethod, HttpStatusCode expectedHttpStatusCode, string expectedContentType)
+        {
+            // arrange
+            RestClient client = new RestClient(baseUrl);
+            RestRequest createRequest = new RestRequest(requestUrl, requestMethod);            
+            var post = Post.NewPost(1, 1, "test title", "test body");
+            createRequest.AddJsonBody(post);
+
+            // act
+            IRestResponse response = client.Execute(createRequest);
+
+            // assert
+            Assert.That(response.StatusCode, Is.EqualTo(expectedHttpStatusCode));
+            Assert.That(response.ContentType, Is.EqualTo(expectedContentType));
+        }
+
+        [TestCase("http://jsonplaceholder.typicode.com/", "posts/1", Method.DELETE, HttpStatusCode.OK, TestName = "Delete Post")]
+        public void DeletePost(string baseUrl, string requestUrl, Method requestMethod, HttpStatusCode expectedHttpStatusCode)
+        {
+            // arrange
+            RestClient client = new RestClient(baseUrl);
+            RestRequest deleteRequest = new RestRequest(requestUrl, requestMethod);
+            RestRequest getRequest = new RestRequest(requestUrl, Method.GET);
+
+
+            // act
+            // check resource exists
+            IRestResponse getResponse = client.Execute(getRequest);
+            Assert.That(getResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+            // Delete
+            IRestResponse deleteResponse = client.Execute(deleteRequest);
+            // check delete 
+            Assert.That(deleteResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+            IRestResponse getResponseAfterDelete = client.Execute(getRequest);
+
+            // assert
+            Assert.That(getResponseAfterDelete.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));            
+        }
+    }
+}
---
+++ ModelObjects/Post.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiTests.ModelObjects
+{
+    public class Post
+    {
+        [JsonProperty("userId")]
+        public int UserId { get; set; }
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("title")]
+        public string Title { get; set; }
+        [JsonProperty("body")]
+        public string Body { get; set; }
+
+        public Post()
+        {
+
+        }
+
+        public static Post NewPost(int userId, int postId, string title, string body) => new Post()
+        {
+            UserId = userId,
+            Id = postId,
+            Title = title,
+            Body = body
+        };
+
+    }
+}
